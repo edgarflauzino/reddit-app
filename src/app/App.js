9f9cbd0224632a088915d74f6a37738895bc8371
Edgar Flauzino <edgarflauzino@gmail.com>
2023-03-12 12:50:05 -0300

    first commit in style branch

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AllPosts } from '../features/allPosts/AllPosts.js';
 import { SearchTerm } from '../features/searchTerm/SearchTerm.js';
+import styles from '../styles/App.css'
 
 export default function App() {
   return (
